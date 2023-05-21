de94bf3c77d238c0f06f9fceb87e811d4f07e2aa
João Victor Fernandes <jvsfernandes924@gmail.com>
2023-05-21 09:55:05 -0300

    feat: added validation for inserts

--- src/infra/databases/scripts/Neo4jScripts.py
+++ src/infra/databases/scripts/Neo4jScripts.py
@@ -4,12 +4,18 @@ class Neo4jScripts:
   CREATE_INDEX_PATENT_CLASSIFICATION = "CREATE INDEX IF NOT EXISTS ON :patent(classification)"
   CREATE_INDEX_PATENT_REGISTERED_DATE = "CREATE INDEX IF NOT EXISTS ON :patent(registered_at)"
   CREATE_CONSTRAINT_PATENT_ID = "CREATE CONSTRAINT patents IF NOT EXISTS ON (p:patent) ASSERT p.patent_id IS UNIQUE"
+  FIND_PATENT_BY_ID = "MATCH (p:patent {patent_id: $patent_id}) RETURN p"
   CREATE_RELATIONSHIP_BETWEEN_NODES = """
-                        UNWIND $rows AS row 
-                        MERGE(:patent {patent_id: row.patentId, author: row.author, classification: row.classification, registered_at: row.registeredAt}) 
-                        WITH p, row
-                        WHERE row.relatedPatentId IS NOT NULL
-                        MATCH (relatedP:patent {patent_id: row.relatedPatentId})
-                        CREATE (p)-[:RELATED_TO]->(relatedP)
-                        RETURN count(*) as total
+                      UNWIND $rows AS row
+                      MERGE (p:patent {patent_id: row.patentId})
+                      ON CREATE SET p += {
+                        author: row.author,
+                        classification: row.classification,
+                        registered_at: row.registeredAt
+                      }
+                      WITH p, row
+                      WHERE row.relatedPatentId IS NOT NULL
+                      MATCH (relatedP:patent {patent_id: row.relatedPatentId})
+                      MERGE (p)-[:RELATED_TO]->(relatedP)
+                      RETURN count(*) as total
                       """
--- src/infra/databases/scripts/PostgreSqlScripts.py
+++ src/infra/databases/scripts/PostgreSqlScripts.py
@@ -1,10 +1,30 @@
 class PostgreSqlScripts:
-  CREATE_TABLE_CITATION = "CREATE TABLE IF NOT EXISTS citation(from_id INT NOT NULL, to_id INT NOT NULL, CONSTRAINT fk_from_patent_id FOREIGN KEY(from_id) REFERENCES patent(patent_id), CONSTRAINT fk_to_patent_id FOREIGN KEY(to_id) REFERENCES patent(patent_id))"
-  CREATE_TABLE_PATENT = "CREATE TABLE IF NOT EXISTS patent(patent_id INT PRIMARY KEY NOT NULL UNIQUE, author VARCHAR(60) NOT NULL, classification VARCHAR(50) NOT NULL, registered_at TIMESTAMP NOT NULL)"
+  CREATE_TABLE_CITATION = """
+                             CREATE TABLE IF NOT EXISTS citation(
+                               from_id INT NOT NULL, 
+                               to_id INT NOT NULL, 
+                               CONSTRAINT fk_from_patent_id FOREIGN KEY(from_id)                   
+                               REFERENCES patent(patent_id), 
+                                  CONSTRAINT fk_to_patent_id FOREIGN KEY(to_id) 
+                               REFERENCES patent(patent_id))
+                          """
+  CREATE_TABLE_PATENT = """
+                           CREATE TABLE IF NOT EXISTS patent(
+                             patent_id INT PRIMARY KEY NOT NULL UNIQUE, 
+                             author VARCHAR(60) NOT NULL, 
+                             classification VARCHAR(50) NOT NULL, 
+                             registered_at TIMESTAMP NOT NULL)
+                        """
   CREATE_INDEX_PATENT_ID = "CREATE INDEX IF NOT EXISTS idx_patent_id on patent USING btree (patent_id)"
   CREATE_INDEX_PATENT_AUTHOR = "CREATE INDEX IF NOT EXISTS idx_patent_author on patent(author)"
   CREATE_INDEX_PATENT_CLASSIFICATION = "CREATE INDEX IF NOT EXISTS idx_patent_classification on patent(classification)"
   CREATE_INDEX_PATENT_REGISTERED_DATE = "CREATE INDEX IF NOT EXISTS idx_patent_registered on patent(registered_at)"
-  INSERT_INTO_PATENT = "INSERT INTO patent (patent_id, author, classification, registered_at) VALUES(%s,%s,%s,%s)"
+  INSERT_INTO_PATENT = """
+                          INSERT INTO patent (patent_id, author, classification, registered_at)
+                          SELECT %s, %s, %s, %s
+                          WHERE NOT EXISTS (
+                            SELECT 1 FROM patent WHERE patent_id = %s
+                          )
+                       """
   INSERT_INTO_CITATION = "INSERT INTO citation (from_id, author, to_id) VALUES(%s,%s)"
   FIND_PATENT_BY_ID = "SELECT * FROM patent WHERE patent_id = %s"
